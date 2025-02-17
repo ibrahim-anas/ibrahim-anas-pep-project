8b8dda7031ecf8e885716f21f0afc5456d6371f4
Anas Ibrahim <ibrahim-anas@users.noreply.github.com>
2025-02-17 19:58:56 +0000

    Add functionality to persist a message

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,5 +1,40 @@
 package DAO;
 
+import Model.Message;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+
 public class MessageDAO {
     
+    /**
+     * Inserts a message into the message table.
+     * @param message
+     * @return The successfully added message
+     */
+    public Message addMessage(Message message) {
+        Connection connection = ConnectionUtil.getConnection();
+
+        try {
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?,?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setInt(1, message.getPosted_by());
+            ps.setString(2, message.getMessage_text());
+            ps.setLong(3, message.getTime_posted_epoch());
+            
+            ps.executeUpdate();
+            ResultSet generatedKeys = ps.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                int message_id = generatedKeys.getInt("message_id");
+                return new Message(message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,24 @@
 package Service;
 
+import Model.Message;
+import DAO.MessageDAO;
+import DAO.AccountDAO;
+
 public class MessageService {
     
+    private MessageDAO messageDAO;
+    private AccountDAO accountDAO;
+
+    public MessageService() {
+        this.messageDAO = new MessageDAO();
+        this.accountDAO = new AccountDAO();
+    }
+
+    public Message createMessage(Message message) {
+        if (message.getMessage_text().length() > 0 && message.getMessage_text().length() < 255 && this.accountDAO.getAccountByID(message.getPosted_by()) != null) {
+            return messageDAO.addMessage(message);
+        }
+        
+        return null;
+    }
 }

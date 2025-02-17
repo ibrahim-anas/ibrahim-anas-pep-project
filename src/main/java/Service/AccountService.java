34ba5038ae118c84f98235646d2a1b42549c8b58
Anas Ibrahim <ibrahim-anas@users.noreply.github.com>
2025-02-17 07:44:45 +0000

    Add functionality to persist a new account

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,25 @@
 package Service;
 
+import DAO.AccountDAO;
+import Model.Account;
+
 public class AccountService {
-    
+    AccountDAO accountDAO;
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = new AccountDAO();
+    }
+
+    /**
+     * Persists a new account.
+     * @param account An account object
+     * @return The successfully created account
+     */
+    public Account createAccount(Account account) {
+        if (account.getUsername().length() > 0 && account.getPassword().length() >= 4 && this.accountDAO.getAccountByUsername(account.getUsername()) != null) {
+            return this.accountDAO.insertAccount(account);
+        }
+
+        return null;
+    }
 }
